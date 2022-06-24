b3afacf47d09cf9802a446ecd0e719336aa475db
mariekart42 <schwokelbaer@gmail.com>
2022-06-24 15:28:31 +0200

    ALL GOOD

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mmensing <mmensing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 22:14:45 by mmensing          #+#    #+#             */
-/*   Updated: 2022/06/24 15:00:48 by mmensing         ###   ########.fr       */
+/*   Updated: 2022/06/24 15:27:22 by mmensing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFFER_SIZE 20
+//#define BUFFER_SIZE 20
 
 # include <stdlib.h>
 # include <unistd.h>
